9ea013c9dd77843cecf3ad99d31fbce09b44e1dd
gyuwonh-ai <gyuwonh95@gmail.com>
2025-11-19 10:25:26 +0900

    commit

--- LegPull_EDA.py
+++ LegPull_EDA.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('dataset\LegPull\LegPull -_cam1\keypoints_필라테스_가산_A__Leg Pull_고급_actorP061_20220829_11.23.50_CAM_1.csv')
+df = pd.read_csv('keypoint_LegPull/norm/norm_필라테스_가산_A__Leg Pull_고급_actorP061_20220829_12.21.09_CAM_1.csv')
 print(df.columns)
 
